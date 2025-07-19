0bd616d56f8bc7993c0bc71f32ad00dd6673b80b
Idrissa Iyamuremye <idrissa.iyamuremye@ozu.edu.tr>
2025-07-19 16:30:27 +0300

    Added all Result.

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ btnRock.addEventListener("click", ()=>{
 }
 alert(`You picked rock. computer picked ${computerMove} ${result}`)
 })
+btnPaper.addEventListener("click", ()=>{
+    if (computerMove ==="rock"){
+    result = "You lose";
+}else if (computerMove === "paper"){
+    result = "Tie"
+}else if (computerMove ==="scissors"){
+    result = "You lose"
+}
+alert(`You picked paper. computer picked ${computerMove} ${result}`)
+})
+btnScissors.addEventListener("click", ()=>{
+    if (computerMove ==="rock"){
+    result = "You win";
+}else if (computerMove === "paper"){
+    result = "You Win"
+}else if (computerMove ==="scissors"){
+    result = "Tie"
+}
+alert(`You picked Scissors. computer picked ${computerMove} ${result}`)
+})
 if (randomNumber>=0 && randomNumber<1/3){
     computerMove  ="rock";
 
